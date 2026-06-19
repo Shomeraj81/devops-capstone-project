888eebe35d575a0c77b1ef84e2c502d7894db02c
Shomeraj81 <your-github-email@example.com>
2026-06-19 08:23:27 -0400

    added delete account endpoint

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -147,7 +147,7 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(len(data), len(accounts))
-        
+
     def test_update_account(self):
         """It should Update an existing Account"""
         account = self._create_accounts(1)[0]
@@ -169,6 +169,17 @@ class TestAccountService(TestCase):
             content_type="application/json",
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        
+    def test_delete_account(self):
+        """It should Delete an Account"""
+        account = self._create_accounts(1)[0]
+        response = self.client.delete(f"{BASE_URL}/{account.id}")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_delete_account_not_found(self):
+        """It should give 204 even if Account does not exist"""
+        response = self.client.delete(f"{BASE_URL}/0")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
 
